786f8eec50763d5a8b69693231eb12e2ebff27b2
Akshat Gupta <akshatguptamac@gmail.com>
2022-11-25 03:51:31 +0530

    Created model message

--- branch_support/chat_support/models.py
+++ branch_support/chat_support/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Message(models.Model):
+
+    user_id = models.IntegerField(default=0) # Assumed to be unique 
+    message_text = models.TextField(max_length=200)
+    timestamp = models.DateTimeField()
+    parent_id = models.ForeignKey("Message", on_delete=models.CASCADE, null=True)
+    is_reply = models.BooleanField(default=False)
+
+
