2f9bb9bfd955eee2ff8d018c6037030d59eb4fb4
Akshat Gupta <akshatguptamac@gmail.com>
2022-11-26 01:02:22 +0530

    - Created send_msg API - Tested send_msg API - Added path for send_msg API in urls.py - Return msg_id in response of send_msg API - Created receive_msg API - Tested receive_msg API - Added path for receive_msg API in urls.py

--- branch_support/chat_support/urls.py
+++ branch_support/chat_support/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from chat_support.views import send_msg, receive_reply
 
 urlpatterns = [
-    
+    path('send_msg/', send_msg, name="send_msg"),
+    path('receive_reply/<int:user_id>/<int:msg_id>/', receive_reply, name="receive_reply")
 ]
--- branch_support/chat_support/views.py
+++ branch_support/chat_support/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
+from chat_support.models import Message
+import json
 
+app_name = "chat_support"
 # Create your views here.
+
+def send_msg(request):
+
+    if request.method == "POST":
+
+        data = request.POST
+        print("HHEHHHE", data)
+        user_id = data.get("user_id")
+        message_text = data.get("message_text")
+        timestamp = timezone.now()
+
+        message = Message.objects.create(user_id=user_id, message_text=message_text,timestamp=timestamp, parent_id=None, is_reply=False)
+        
+        ctx = {
+            "msg_id" : message.id
+        }
+        
+        return HttpResponse(json.dumps(ctx), status=201)
+    
+    return HttpResponse(status=401)
+
+
+def receive_reply(request, user_id, msg_id):
+ 
+    if request.method == "GET":
+
+        data = Message.objects.filter(user_id=user_id, parent_id_id=msg_id).first()
+
+        if data is not None:
+            timestamp = data.timestamp
+            timestamp_str = timestamp.strftime("%D, %H:%M:%S") 
+
+            ctx = {
+                "user_id": user_id,
+                "reply_message":data.message_text,
+                "timestamp": timestamp_str,
+                "reply_ready": True
+            }
+        else:
+            ctx = {
+                "user_id": None,
+                "reply_message":None,
+                "timestamp":None,
+                "reply_ready": False
+            }
+
+        return HttpResponse(json.dumps(ctx), content_type="application/json", status=200)
+
